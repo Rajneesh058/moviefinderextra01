805fe5b649bc51b771567b3663dcd88424381b30
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-11-29 13:02:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 # Bot information
 PORT = environ.get("PORT", "8080")
 SESSION = environ.get('SESSION')
-API_ID = int(environ('API_ID')
-API_HASH = environ('API_HASH')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
 BOT_TOKEN = environ('BOT_TOKEN')
 
 # Bot settings
